3772a1954c955200e6074474538ab413f7731ab5
Samara Canjura <samara.canjura@gmail.com>
2025-04-11 16:34:48 -0700

    Phase 1 Completed
    Changed minor paths and port to 5001

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM python:3.9-slim
+
 WORKDIR /app
-COPY node.py .
+
+COPY node.py /app
+
+RUN pip install Flask 
+
+EXPOSE 5000
+
 CMD ["python", "node.py"]
---
+++ node.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import uuid #creates the unique id
+import os
+
+#initializes flask app
+app = Flask(__name__)
+
+#generate random id
+node_id = str(uuid.uuid4())
+
+#stores known peers
+peers = set()
+
+#message with id 
+@app.route('/')
+def index():
+    return jsonify({"message": f"Node {node_id} is running!"})
+
+#registers the peer 
+@app.route('/register', methods=['POST'])
+def register_peer():
+    data = request.get_json()
+    peer = data.get('peer')
+    if peer: 
+        peers.add(peer)
+        return jsonify({"status": "peer registered", "peers": list(peers)}), 200
+    return jsonify({"error": "no peer provide"}), 400
+#starts the app
+if __name__== "__main__":
+    app.run(host= "0.0.0.0", port=5000)
+
+
